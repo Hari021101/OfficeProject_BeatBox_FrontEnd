41c798bb779471ead6990f28173e9159aa80eb73
Hari021101 <mhariharan2019@gmail.com>
2026-05-14 20:11:53 +0530

    Update logo and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './index.css'
+import logo from './assets/Logo.png'
 
 function App() {
   return (
-    <div style={{ padding: '2rem' }}>
-      <h1>BeatBox Project</h1>
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <img src={logo} alt="BeatBox Logo" style={{ width: '250px', borderRadius: '15px' }} />
+      <h1 style={{ marginTop: '1rem', color: '#06b6d4' }}>Welcome to BeatBox</h1>
       <p>Clean slate ready! Start building here.</p>
     </div>
   )
